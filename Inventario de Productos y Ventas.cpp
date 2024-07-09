752cfef2c22420992146b154073416896d2bc0d4
Awpq2024 <awpariq@unjbg.edu.pe>
2024-07-08 19:23:55 -0500

    Se agrega el ejercicio de Productos y ventas, con las funcionesiniciales y principales de registar un producto y una venta mediante el menu mostrado, hasta que seleccione la opcion de salida

---
+++ Inventario de Productos y Ventas.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+using namespace std;
+
+const int limiteProductos=100;
+const int limiteVentas=100;
+
+struct Producto{
+	string nombre;
+	float precio;
+};
+struct Venta{
+	int idVenta;
+	string producto;
+	int cantidad;
+    float precioTotal;
+};
+
+void registrarProducto(Producto articulo[],int &numeroProducto){
+	if (numeroProducto>=limiteProductos){
+        cout<<"limite de productos alcanzado, no se puede registrar mas productos"<<endl;
+        return;
+	}
+	cout<<"Ingrese el nombre del producto: ";
+	cin>>articulo[numeroProducto].nombre;
+	cout<<"Ingrese el precio del producto: ";
+	cin>>articulo[numeroProducto].precio;
+	numeroProducto++;
+}
+
+void registrarVenta(Venta transaccion[], int &numeroVenta, int &idVentaActual){
+ 	if (numeroVenta>=limiteVentas){
+        cout<<"Limite de ventas alcanzado, no se puede registrar mas Ventas"<<endl;
+        return;
+    }
+	transaccion[numeroVenta].idVenta=idVentaActual++;
+	cout<<"Introdusca el nombre del producto que fue vendido: ";
+	cin>>transaccion[numeroVenta].producto;
+	cout<<"Ingrese la cantidad vendida del producto: ";
+	cin>>transaccion[numeroVenta].cantidad;
+	cout<<"Ingrese el precio total por la venta: ";
+	cin>>transaccion[numeroVenta].precioTotal;
+	numeroVenta++;
+}
+
+int main(){
+	Producto articulo[limiteProductos];
+	Venta transaccion[limiteVentas];
+	int numeroProducto=0;
+	int numeroVenta=0;
+	int idVentaActual=1;
+	int opcion;
+	do{
+		cout<<"Menu de inventario y ventas"<<endl;
+		cout<<"1. Registrar un nuevo producto"<<endl;
+		cout<<"2. Registrar una venta"<<endl;
+		cout<<"3. Salir del programa"<<endl;
+		cout<<"Seleccione una opcion: ";
+		cin>>opcion;
+		switch (opcion){
+			case 1:
+				registrarProducto(articulo, numeroProducto);
+				break;
+			case 2:
+				registrarVenta(transaccion, numeroVenta, idVentaActual);
+				break;
+			case 3:
+				cout<<"Saliendo del programa...";
+				break;
+			default:
+				cout<<"Opcion no valida, seleccione una opcion del 1 al 3."<<endl;
+				break;
+		}
+	}while(opcion!=3);
+	return 0;
+}
